a1fa9c1c239cfe95783f0b2f08bf7fc6950bebb2
jorge-imperial <jorgeluis.imperial@gmail.com>
2022-12-15 10:29:25 -0800

    Initial

---
+++ src/main/java/com/ausfaller/examples/clustermonitor/ClusterMonitorApplication.java
@@ -0,0 +1,128 @@
+package com.ausfaller.examples.clustermonitor;
+
+
+
+import com.mongodb.*;
+import nl.altindag.ssl.SSLFactory;
+import nl.altindag.ssl.util.PemUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509ExtendedKeyManager;
+import javax.net.ssl.X509ExtendedTrustManager;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+
+
+// This disables default host monitoring (DefaultServerMonitor)
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
+public class ClusterMonitorApplication implements CommandLineRunner  {
+
+    private static final int THREAD_COUNT = 100;
+
+    List<ServerAddress> atlasMongos = Arrays.asList(new ServerAddress("cs44-shard-00-00.hqulf.mongodb.net", 27016),
+            new ServerAddress("cs44-shard-00-01.hqulf.mongodb.net", 27016),
+            new ServerAddress("cs44-shard-00-02.hqulf.mongodb.net", 27016),
+            new ServerAddress("cs44-shard-01-00.hqulf.mongodb.net", 27016),
+            new ServerAddress("cs44-shard-01-01.hqulf.mongodb.net", 27016),
+            new ServerAddress("cs44-shard-01-02.hqulf.mongodb.net", 27016)
+    );
+
+    List<ServerAddress> onPremHosts = Arrays.asList(new ServerAddress("76.167.199.164", 37017),
+            new ServerAddress("76.167.199.164", 37018),
+            new ServerAddress("76.167.199.164", 37019),
+            new ServerAddress("76.167.199.164", 37020),
+            new ServerAddress("76.167.199.164", 37021),
+            new ServerAddress("76.167.199.164", 37022)
+    );
+
+    private static Logger logger = LogManager.getLogger(ClusterMonitorApplication.class);
+    private static int maxConnections = 50;
+    private static int maxWaitTime = 10000;
+
+    public static void main(String[] args) {
+
+        Properties props = System.getProperties();
+        logger.info("Current working directory is " + props.getProperty("user.dir"));
+
+        SpringApplication.run(ClusterMonitorApplication.class, args);
+    }
+
+    MongoClient mongoClient = null;
+    @Override
+    public void run(String... args) throws Exception {
+
+        MongoCredential credential = MongoCredential.createScramSha1Credential("user", "admin", "password".toCharArray());
+        WorkerThread[] workers = new WorkerThread[THREAD_COUNT];
+
+        Boolean on_prem = true;
+
+        if (on_prem == true) {
+            logger.info("Connecting to on-prem environment");
+
+            // We need an SSL context for on-prem environment using self-signed certificates.
+            X509ExtendedKeyManager keyManager = PemUtils.loadIdentityMaterial(Paths.get("/etc/ssl/M-C756C1J67D.pem"));
+            X509ExtendedTrustManager trustManager = PemUtils.loadTrustMaterial(Paths.get("/etc/ssl/rootCA.pem"));
+            SSLFactory sslFactory = SSLFactory.builder()
+                    .withIdentityMaterial(keyManager)
+                    .withTrustMaterial(trustManager)
+                    .build();
+            SSLContext sslContext = sslFactory.getSslContext();
+
+            MongoClientOptions options = new MongoClientOptions.Builder()
+                    .connectionsPerHost(maxConnections)
+                    .connectTimeout(30000)
+                    .socketTimeout(60000)
+                    .maxWaitTime(maxWaitTime)
+                    .sslEnabled(true)
+                    .sslContext(sslContext)
+                    .sslInvalidHostNameAllowed(true)
+                    .writeConcern(WriteConcern.MAJORITY)
+                    .retryWrites(true)
+                    .readPreference(ReadPreference.primary())
+                    .addServerListener(new MongoServerListener("ONPREM"))
+                    .addConnectionPoolListener(new MongoConnectionListener())
+                    .build();
+
+            mongoClient = new MongoClient(onPremHosts, credential, options);
+        } else {
+
+            MongoClientOptions options = new MongoClientOptions.Builder()
+                    .connectionsPerHost(maxConnections)
+                    .connectTimeout(30000)
+                    .socketTimeout(60000)
+                    .maxWaitTime(maxWaitTime)
+                    .sslEnabled(true)
+                    .writeConcern(WriteConcern.MAJORITY)
+                    .retryWrites(true)
+                    .readPreference(ReadPreference.primary())
+                    .addServerListener(new MongoServerListener("ATLAS"))
+                    .addConnectionPoolListener(new MongoConnectionListener())
+                    .build();
+
+
+            mongoClient = new MongoClient(atlasMongos, credential, options);
+        }
+
+        for (int i = 0; i < THREAD_COUNT; ++i) {
+            workers[i] = new WorkerThread(i, mongoClient);
+
+            // ramp up connections
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            workers[i].start();
+        }
+    }
+}
---
+++ src/test/java/com/ausfaller/examples/clustermonitor/ClusterMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ausfaller.examples.clustermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
